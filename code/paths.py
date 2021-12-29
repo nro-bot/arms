2ef5c6cbdb1975975c82e69dc76c995132a966ea
Ondrej Biza <ondrej.biza@gmail.com>
2021-12-29 16:25:36 -0500

    moving arm

--- code/paths.py
+++ code/paths.py
@@ -1,2 +1,9 @@
+import os
+
+
 DEFAULT_CALIBRATION_PATH = "data/calibration.pickle"
 DEFAULT_XARM_CONFIG_PATH = "data/xarm_config.npy"
+
+dir_name = os.path.dirname(__file__)
+URDF_DIR = os.path.join(dir_name, "assets/urdf")
+print(URDF_DIR)
---
+++ code/scr/test.py
@@ -0,0 +1,27 @@
+import time
+import numpy as np
+from robot.robot_arm import RobotArm
+
+
+r = RobotArm()
+r.home()
+r.close_gripper()
+
+r.move_hand_to([0.2, 0.0, 0.05])
+r.open_gripper()
+r.move_hand_to([0.2, 0.0, 0.01])
+r.close_gripper()
+r.home()
+r.open_gripper()
+
+# bottom-left corner = 0.15, 0.1, 0.
+# top-left corner = 0.3, 0.11, 0.
+# top-right corner = 0.3, -0.09, 0.
+# bottom-right corner = 0.15, -0.07, 0.
+
+
+
+# r.passive_mode()
+# while True:
+#     print(r.get_hand_pose())
+#     time.sleep(1)
